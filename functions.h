0bdc40260452a2c9e5a1514f140bcd39aa4dbd31
razvansavescu <39966269+razvansavescu@users.noreply.github.com>
2018-06-05 21:59:09 +0300

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,33 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+
+class Graph
+{
+    int V;    // No. of vertices
+
+    // Pointer to an array containing adjacency lists
+
+    // A function used by DFS
+   void DFSUtil(int v, bool visited[]){
+
+
+    void addEdge(int v, int w);
+    void connectedComponents();
+}
+};
+
+
+ void DFSUtil(int v, bool visited[]){
+
+    void addEdge(int v, int w);
+    void connectedComponents();
+}
+
+
+
+
+
+
+
+#endif
